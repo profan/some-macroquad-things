e233cf38608be1fc480096302959bfe279552184
profan <robinhubner@gmail.com>
2022-09-20 23:00:38 +0100

    corners: draw arrow for direction

--- corners/src/main.rs
+++ corners/src/main.rs
@@ -14,6 +14,46 @@ fn corner_vector(sides: impl Iterator::<Item=Vec2>) -> Option<Vec2> {
 
 }
 
+fn draw_arrow(origin: Vec2, ray: Vec2, arrow_length: f32, thickness: f32) {
+
+    let line_end = origin + ray;
+
+    draw_line(
+        origin.x(),
+        origin.y(),
+        line_end.x(),
+        line_end.y(),
+        thickness,
+        WHITE
+    );
+
+    let p1 = vec2(-ray.y(), ray.x());
+    let p2 = vec2(ray.y(), -ray.x());
+    let left = -ray.lerp(p1, 0.5);
+    let right = -ray.lerp(p2, 0.5);
+    let left_end = line_end + left.normalize() * arrow_length;
+    let right_end = line_end + right.normalize() * arrow_length;
+
+    draw_line(
+        line_end.x(),
+        line_end.y(),
+        left_end.x(),
+        left_end.y(),
+        thickness,
+        WHITE
+    );
+
+    draw_line(
+        line_end.x(),
+        line_end.y(),
+        right_end.x(),
+        right_end.y(),
+        thickness,
+        WHITE
+    );
+
+}
+
 #[macroquad::main("corners")]
 async fn main() {
 
@@ -44,59 +84,41 @@ async fn main() {
 
         clear_background(BLUE);
 
-        if is_key_pressed(KeyCode::W) { top_set = !top_set; };
-        if is_key_pressed(KeyCode::S) { bottom_set = !bottom_set; };
-        if is_key_pressed(KeyCode::A) { left_set = !left_set; };
-        if is_key_pressed(KeyCode::D) { right_set = !right_set; };
+        top_set = is_key_down(KeyCode::W);
+        bottom_set = is_key_down(KeyCode::S);
+        left_set = is_key_down(KeyCode::A);
+        right_set = is_key_down(KeyCode::D);
 
         let screen_center = vec2(screen_width() / 2.0,screen_height() / 2.0);
 
         let corners = [
-            top_set.then_some(vec2(0.0, 1.0)),
-            bottom_set.then_some(vec2(0.0, -1.0)),
-            left_set.then_some(vec2(1.0, 0.0)),
-            right_set.then_some(vec2(-1.0, 0.0))
+            top_set.then_some(vec2(0.0, -1.0)),
+            bottom_set.then_some(vec2(0.0, 1.0)),
+            left_set.then_some(vec2(-1.0, 0.0)),
+            right_set.then_some(vec2(1.0, 0.0))
         ];
 
         let current_corners = corners.iter().filter_map(|v| *v);
         let current_corner_vector = corner_vector(current_corners);
-        let vector_length = 16.0;
+        let vector_length = 32.0;
 
         if let Some(current_vector) = current_corner_vector {
-            let screen_vector_end = screen_center + current_vector * vector_length;
-            draw_line(
-                screen_center.x(),
-                screen_center.y(),
-                screen_vector_end.x(),
-                screen_vector_end.y(),
-                4.0,
-                WHITE
+
+            let screen_vector = current_vector * vector_length;
+
+            draw_arrow(
+                screen_center,
+                screen_vector,
+                16.0, // arrow length
+                4.0
             );
             
-        }
-
-        let box_size = vector_length;
-
-        if top_set {
-            draw_box(screen_center.x(), screen_center.y() - box_size, box_size);
-        }
-
-        if bottom_set {
-            draw_box(screen_center.x(), screen_center.y() + box_size, box_size);
-        }
-
-        if left_set {
-            draw_box(screen_center.x() - box_size, screen_center.y(), box_size);
-        }
-
-        if right_set {
-            draw_box(screen_center.x() + box_size, screen_center.y(), box_size);
         }
 
         draw_debug_text(format!("top set: {} (W)", top_set), 32.0, 32.0);
         draw_debug_text(format!("bottom set: {} (S)", bottom_set), 32.0, 48.0);
         draw_debug_text(format!("left set: {} (A)", left_set), 32.0, 64.0);
-        draw_debug_text(format!("right set: {} (D)", top_set), 32.0, 80.0);
+        draw_debug_text(format!("right set: {} (D)", right_set), 32.0, 80.0);
 
         next_frame().await
 
