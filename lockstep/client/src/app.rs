e7281df3a7c1269997af33af84f01c194fe0182a
profan <robinhubner@gmail.com>
2023-11-18 21:35:02 +0000

    lockstep - client: fix display formatting

--- lockstep/client/src/app.rs
+++ lockstep/client/src/app.rs
@@ -375,7 +375,7 @@ impl<GameType> ApplicationState<GameType> where GameType: Game {
 
                         for lobby in self.relay.get_lobbies() {
 
-                            let lobby_text = format!("{} ({}) - {}", lobby.name, lobby.id, lobby.state);
+                            let lobby_text = format!("{} ({}) - {:?}", lobby.name, lobby.id, lobby.state);
                             yakui_min_row(|| {
                                 yakui::label(lobby_text);
                                 if lobby.state == LobbyState::Open && yakui::button("join").clicked {
