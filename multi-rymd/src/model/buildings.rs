8736b6209aa9dc2e44252285b9f0b2455cdcdc64
Robin Hübner <robinhubner@gmail.com>
2023-12-16 21:34:07 +0000

    multi-rymd: adjust shipyard sprite a little bit, make a v2

--- multi-rymd/src/model/buildings.rs
+++ multi-rymd/src/model/buildings.rs
@@ -108,7 +108,7 @@ pub fn build_shipyard(world: &mut World, owner: PlayerID, position: Vec2) -> Ent
 
     let controller = Controller { id: owner };
     let transform = Transform::new(position, 0.0, None);
-    let spawner = Spawner { position: vec2(-(shipyard_size / 3.0), 0.0) };
+    let spawner = Spawner { position: vec2(-(shipyard_size / 5.0), 0.0) };
     let orderable = Orderable::new();
     let constructor = Constructor { is_constructing: false, constructibles: vec![2], build_range: shipyard_size as i32 / 2, build_speed: 100, beam_offset: -vec2(0.0, 8.0) };
     let health = Health { full_health: full_shipyard_health, current_health: initial_shipyard_health };
