e82d5fc57c90f6161c8119021c02e3f13d4e594f
Robin Hübner <robinhubner@gmail.com>
2024-03-03 13:26:47 +0000

    multi-rymd: small refactoring in projectile creator code

--- multi-rymd/src/model/projectiles.rs
+++ multi-rymd/src/model/projectiles.rs
@@ -35,15 +35,18 @@ pub fn create_simple_bullet(world: &mut World, owner: PlayerID, position: Vec2, 
     let simple_bullet_velocity = 256.0;
     let simple_bullet_damage = 10.0;
 
-    let simple_bullet_bounds = Rect { x: 0.0, y: 0.0, w: 2.0, h: 2.0 };
+    let is_static = false;
+    let is_enabled = true;
+    let bounds = Rect { x: 0.0, y: 0.0, w: 2.0, h: 2.0 };
+    let mask = 1 << owner;
     
     let orientation = -direction.as_angle();
-    let body = create_default_kinematic_body(position, orientation);
+    let kinematic = create_default_kinematic_body(position, orientation);
 
     let controller = Controller { id: owner };
     let transform = Transform::new(position, orientation, None);
     let sprite = Sprite { texture: "SIMPLE_BULLET".to_string() };
-    let dynamic_body = DynamicBody { is_static: false, is_enabled: true, kinematic: body, bounds: simple_bullet_bounds, mask: 1 << owner };
+    let dynamic_body = DynamicBody { is_static, is_enabled, bounds, kinematic, mask };
     let dynamic_body_callback = DynamicBodyCallback { on_collision: on_bullet_impact };
     let projectile = Projectile { damage: simple_bullet_damage, lifetime: simple_bullet_lifetime, velocity: simple_bullet_velocity };
     let health = Health::new(simple_bullet_health);
